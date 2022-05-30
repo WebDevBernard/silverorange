c7358cdace8c12c8f8565036902e7fe3848bd5da
Bernard Yang <ybernard@gmail.com>
2022-05-29 19:48:49 -0700

    testing custom react hook in browser

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,7 @@
+import useFetchRepos from './hooks/useFetchRepos';
 export function App() {
-  return <h1 className="text-3xsl font-bold underline">Hello world!</h1>;
+  const { repos } = useFetchRepos();
+  console.log(repos);
+
+  return <h1 className="text-3xsl font-bold underline">Test</h1>;
 }
