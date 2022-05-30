76cfa5785e8af516efdacb19acab88efe50a70e4
Bernard Yang <ybernard@gmail.com>
2022-05-29 18:42:51 -0700

    add api fetch with AXIOS, combined array resulted unfiltered

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,14 +1,29 @@
+import axios from 'axios';
+import { Repo } from '../models/Repo';
 import { Router, Request, Response } from 'express';
 const fs = require('fs');
 const path = require('path');
 export const repos = Router();
 
+// fetch from api repo
+const getApiRepos = async () => {
+  try {
+    const response = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+
+    response.data.map((repo: Repo) => getRepos.push(repo));
+  } catch (err) {
+    console.log(err);
+  }
+};
+getApiRepos();
 // fetch from local repo
 const localRepoPath = fs.readFileSync(
   path.resolve(__dirname, '../../data/repos.json')
 );
 
-const localRepo = JSON.parse(localRepoPath);
+const getRepos: Repo[] = JSON.parse(localRepoPath);
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -16,5 +31,5 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([localRepo]);
+  res.json({ getRepos });
 });
