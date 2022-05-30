ad3c26d8f97ef481eb79a580e37dd273e1339110
Bernard Yang <ybernard@gmail.com>
2022-05-30 00:12:14 -0700

    add onclick to table to console log the commit url object

--- web/src/components/List.tsx
+++ web/src/components/List.tsx
@@ -1,36 +1,42 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { nanoid } from 'nanoid';
-import { IProps } from '../store/interface';
+import { Repo, Commit } from '../store/interface';
 
 // repository name, description,
 //     language, and forks count in the list.
 
-const List: FC<{ repos: IProps[] }> = ({ repos }) => {
+const List: FC<{
+  repos: Repo[];
+  commitInfo: Commit[];
+  handleChange: (url: string) => void;
+}> = ({ repos, commitInfo, handleChange }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Language</th>
-          <th>Forks Count</th>
-          <th>Created At</th>
-        </tr>
-      </thead>
-      <tbody>
-        {repos.map((repo) => {
-          return (
-            <tr key={nanoid()}>
-              <td>{repo.name}</td>
-              <td>{repo.description}</td>
-              <td>{repo.language}</td>
-              <td>{repo.forks_count}</td>
-              <td>{repo.created_at}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Forks Count</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {repos.map((repo) => {
+            return (
+              <tr key={nanoid()} onClick={() => handleChange(repo.url)}>
+                <td>{repo.name}</td>
+                <td>{repo.description}</td>
+                <td>{repo.language}</td>
+                <td>{repo.forks_count}</td>
+                <td>{repo.created_at}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
