e2383ed386b30a24c7f92428a1315b90b3c9d75a
Bernard Yang <ybernard@gmail.com>
2022-05-29 19:14:20 -0700

    add typescript interface for fetched data

---
+++ web/src/store/interface.ts
@@ -0,0 +1,8 @@
+// 2. Display a list of repositories. Include the repository name, description,
+// language, and forks count in the list.
+export interface IProps {
+  name: string;
+  description: string;
+  language: string;
+  fork: number;
+}
