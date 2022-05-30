237407d6ce56eb7cf1c9f550dda7685c228713c5
Bernard Yang <ybernard@gmail.com>
2022-05-29 18:47:44 -0700

    add filter function to return data only for repo.fork === false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -18,6 +18,7 @@ const getApiRepos = async () => {
   }
 };
 getApiRepos();
+
 // fetch from local repo
 const localRepoPath = fs.readFileSync(
   path.resolve(__dirname, '../../data/repos.json')
@@ -25,11 +26,15 @@ const localRepoPath = fs.readFileSync(
 
 const getRepos: Repo[] = JSON.parse(localRepoPath);
 
+// helper function -- Only return repositories where `repository.fork` is `false`
+
+const filterForRepoFork = (i: Repo[]) => i.filter((repo) => !repo.fork);
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json({ getRepos });
+  res.json({ data: filterForRepoFork(getRepos) });
 });
