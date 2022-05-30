c208134beff541b8090142fb69a225a576ea8709
Bernard Yang <ybernard@gmail.com>
2022-05-29 18:36:55 -0700

    add fs read for JSON file

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,6 +1,14 @@
 import { Router, Request, Response } from 'express';
-
+const fs = require('fs');
+const path = require('path');
 export const repos = Router();
+
+// fetch from local repo
+const localRepoPath = fs.readFileSync(
+  path.resolve(__dirname, '../../data/repos.json')
+);
+
+const localRepo = JSON.parse(localRepoPath);
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -8,5 +16,5 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json([localRepo]);
 });
