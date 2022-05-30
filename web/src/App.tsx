6304067d6c3d2d61ae2851bb08010391ab246f03
Bernard Yang <ybernard@gmail.com>
2022-05-29 21:29:14 -0700

    renamed components after Eslint rule change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,16 @@
 import useFetchRepos from './hooks/useFetchRepos';
-import Buttons from './components/Buttons';
-import Table from './components/Table';
-import Readme from './components/Readme';
+import RepoButtons from './components/Buttons';
+import List from './components/List';
+import Repo from './components/Repo';
 export function App() {
   const { repos } = useFetchRepos();
   console.log(repos);
 
   return (
     <div className="flex flex-col items-center">
-      <Buttons />
-      <Table repos={repos} />
-      <Readme />
+      <RepoButtons repos={repos} />
+      <List repos={repos} />
+      <Repo />
     </div>
   );
 }
