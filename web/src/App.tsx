1f8f68735de9fffe584b2a6162409a3cb7078b78
Bernard Yang <ybernard@gmail.com>
2022-05-30 00:09:40 -0700

    pass handle change function from custom hook to app to list component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,14 @@
 import useFetchRepos from './hooks/useFetchRepos';
 import RepoButtons from './components/Buttons';
 import List from './components/List';
-import Repo from './components/Repo';
+
 export function App() {
-  const { repos } = useFetchRepos();
-  console.log(repos);
+  const { repos, handleChange, commitInfo } = useFetchRepos();
 
   return (
     <div className="flex flex-col items-center">
       <RepoButtons repos={repos} />
-      <List repos={repos} />
-      <Repo />
+      <List repos={repos} handleChange={handleChange} commitInfo={commitInfo} />
     </div>
   );
 }
